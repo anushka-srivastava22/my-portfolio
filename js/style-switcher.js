294afe9560333100e55942733ebe5212eec24213
Anushka Srivastava <67271376+anushka-srivastava22@users.noreply.github.com>
2025-06-14 12:55:35 +0530

    Update style-switcher.js

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -23,17 +23,17 @@ function setActiveStyle(color){
 
 const dayNight = document.querySelector(".day-night");
 dayNight.addEventListener("click", ()=>{
-    dayNight.querySelector("i").classList.toggle("fa-sun");
     dayNight.querySelector("i").classList.toggle("fa-moon");
+    dayNight.querySelector("i").classList.toggle("fa-sun");
     document.body.classList.toggle("light");
 })
 window.addEventListener("load", ()=>{
     if(document.body.classList.contains("light"))
     {
-        dayNight.querySelector("i").classList.add("fa-sun");
+        dayNight.querySelector("i").classList.add("fa-moon");
     }
     else 
     {
-        dayNight.querySelector("i").classList.add("fa-moon");
+        dayNight.querySelector("i").classList.add("fa-sun");
     }
 })
